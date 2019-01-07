df1791c243dd4832f35345d93e7ef39c5ce2a528
YS <ghys@users.noreply.github.com>
2019-01-07 16:53:55 +0100

    Fix sitemaps

--- src/pages/sitemap.vue
+++ src/pages/sitemap.vue
@@ -4,12 +4,17 @@
       <f7-nav-left v-if="sitemapId === pageId">
         <f7-link icon-if-ios="f7:menu" icon-if-md="material:menu" panel-open="left"></f7-link>
       </f7-nav-left>
-      <f7-nav-title>Sitemap</f7-nav-title>
+      <f7-nav-title v-if="sitemap.title">{{sitemap.title}}</f7-nav-title>
     </f7-navbar>
-    <f7-block class="block-narrow">
+    <f7-block class="block-narrow" v-if="sitemap.widgets">
       <f7-row>
         <f7-col>
-          <sitemap-widget-generic :model="sitemap" :sitemapId="sitemapId" :pageId="pageId" />
+          <f7-list v-if="sitemap.widgets[0].type !== 'Frame'">
+            <ul>
+              <sitemap-widget-generic :model="sitemap" :sitemapId="sitemapId" :pageId="pageId" />
+            </ul>
+          </f7-list>
+          <sitemap-widget-generic v-else :model="sitemap" :sitemapId="sitemapId" :pageId="pageId" />
         </f7-col>
       </f7-row>
     </f7-block>
